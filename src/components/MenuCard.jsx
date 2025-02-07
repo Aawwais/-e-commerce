76a02d9e821b0b3a28ce5ffa833bc8559c1a267c
Awais <Awaisali@80.com>
2025-02-07 08:28:46 +0500

    update file names

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const MenuCard = () => {
   return (
-    <div className="max-w-sm cursor-pointer rounded overflow-hidden shadow-lg bg-white transform transition duration-500 hover:scale-110 hover:bg-black hover:bg-opacity-5 hover:inset-0 hover:border-4 hover:border-customGreen ">
+    <div className="max-w-sm cursor-pointer rounded overflow-hidden shadow-lg bg-white transform transition duration-500 hover:scale-110 hover:bg-black hover:bg-opacity-5 hover:inset-0 hover:border-4 hover:border-customGreen relative min-h-[350px] ">
       <Image className="w-full" src={img} alt="Card image" />
       <div className="px-6 py-4 text-center">
         <div className="font-bold text-xl mb-2">Card Title</div>
